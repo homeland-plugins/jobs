d5aa5687a7b3b18e6f5e765491b786be97f4702b
Jason Lee <huacnlee@gmail.com>
2018-04-11 21:17:00 +0800

    Version 0.2.0

--- lib/homeland/jobs/version.rb
+++ lib/homeland/jobs/version.rb
@@ -1,7 +1,7 @@
 module Homeland
   module Jobs
-    NAME    = 'jobs'
+    NAME = 'jobs'
     DESCRIPTION = 'Display Job channel in navbar for list jobs.'
-    VERSION = '0.1.1'
+    VERSION = '0.2.0'
   end
 end
